4a296b207ab63e9d90e2a8c4d1c4decc5c025597
dantsub <dany.mrtinez@gmail.com>
2020-01-04 19:54:33 -0500

    add math

---
+++ math.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+/**
+ * _add - adds the top two elements of the stack.
+ * @stack: stack.
+ * @line_number: line_number.
+ * Return: Nothing.
+ */
+void _add(stack_t **stack, unsigned int line_number)
+{ 
+	int sum;
+
+	if (!*stack || !(*stack)->next)
+	{
+		if (*stack)
+			free_stack(stack);
+		err_line(line_number, "add", 4);
+	}
+	sum = (*stack)->n + (*stack)->next->n;
+	_pop(*stack, line_number);
+	(*stack)->n = sum;
+}
