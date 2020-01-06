ce9db7adb8258719c837470d81ef5fa76290f0ad
dantsub <dany.mrtinez@gmail.com>
2020-01-06 18:03:17 -0500

    correction conditional

--- char_str_rot.c
+++ char_str_rot.c
@@ -21,9 +21,9 @@ void _pstr(stack_t **stack, unsigned int line_number)
 	stack_t *tmp = *stack;
 	(void)line_number;
 
-	if (tmp->n > 0 && tmp->n < 128)
+	if (tmp)
 	{
-		while (tmp->n)
+		while (tmp->n > 0 && tmp->n < 128)
 		{
 			if (tmp->n > 64 && tmp->n < 127)
 				putchar(tmp->n);
@@ -31,4 +31,5 @@ void _pstr(stack_t **stack, unsigned int line_number)
 		}
 		putchar('\n');
 	}
+	// return;
 }
